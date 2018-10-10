624ac4c17c086ce497b2bf1c5a518f33ee9edf90
8ec_2621115449_zhaoxuerong <8ec_2621115449_zhaoxuerong@git.cloud.tencent.com>
2018-10-10 09:47:06 +0800

    火车票数据模版制作

--- pages/search/search.js
+++ pages/search/search.js
@@ -1,10 +1,17 @@
+var traindata = require("search_data/search_data.js");
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    
+    traindata:''
+  },
+  onLoad: function (options) {
+    this.setData({
+      traindata: traindata.traindata
+    })
+
   },
   data_listto: function (e) {
     console.log("111111");
---
+++ pages/search/search_data/search_data.js
@@ -0,0 +1,42 @@
+var traindata = [{
+  //车票信息
+  trainid: '0',
+  traintimes: 'G1122',
+  departurestation: '杭州东',
+  duration: '50分钟',
+  terminal:'上海虹桥',
+
+  //席位信息
+  business: '商务',
+  firstclass: '一等',
+  secondclass: '二等',
+
+  //余票信息
+  residual1: '无',
+  residual2: '无',
+  residual3: '1张',
+
+
+},
+{
+  //车票信息
+  trainid: '1',
+  traintimes: 'G1122',
+  departurestation: '杭州东',
+  duration: '50分钟',
+  terminal: '上海虹桥',
+
+  //席位信息
+  business: '商务',
+  firstclass: '一等',
+  secondclass: '二等',
+
+  //余票信息
+  residual1: '无',
+  residual2: '无',
+  residual3: '1张',
+}]
+
+module.exports = {
+  traindata: traindata
+}
